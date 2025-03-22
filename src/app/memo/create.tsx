46bcbbd42f75e72d5e33328704ba410b731e728f
otkken2 <lovelovezamakko2@gmail.com>
2025-03-22 23:52:44 +0900

    メモ登録画面に登録機能を実装

--- src/app/memo/create.tsx
+++ src/app/memo/create.tsx
@@ -1,28 +1,43 @@
 import {
   View,
   StyleSheet,
-  TextInput,
-  KeyboardAvoidingView
+  TextInput
 } from 'react-native'
 import { CircleButton } from '../../components/CircleButton'
+import KeyboardAvoidingView from '../../components/KeyboardAvoidingView'
 import { Icon } from '../../components/Icon'
 import { router } from 'expo-router'
+import { addDoc, collection, Timestamp } from 'firebase/firestore'
+import { auth, db } from '../../config'
+import { useState } from 'react'
 
-const handlePress = () => {
-  router.back()
+const handlePress = async (bodyText: string) => {
+  if (!auth.currentUser) return
+  const ref = collection(db, `users/${auth.currentUser.uid}/memos`)
+  addDoc(ref, {
+    bodyText,
+    updatedAt: Timestamp.fromDate(new Date())
+  }).then((docRef) => {
+    console.log('success', docRef.id)
+    router.back()
+  }).catch((error) => {
+    console.log(error)
+  })
 }
-
 const Create = () => {
+  const [bodyText, setBodyText] = useState('')
   return (
-    <KeyboardAvoidingView style={styles.container} behavior='height'>
+    <KeyboardAvoidingView style={styles.container}>
       <View style={styles.inputContainer}>
         <TextInput
-          value=''
+          autoFocus
+          value={bodyText}
           style={styles.input}
           multiline
+          onChangeText={(text) => setBodyText(text)}
         />
       </View>
-      <CircleButton onPress={handlePress}>
+      <CircleButton onPress={() => handlePress(bodyText)}>
         <Icon name="check" size={40} color="white" />
       </CircleButton>
     </KeyboardAvoidingView>
