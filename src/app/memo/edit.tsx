c8221bb3b1b685e66063130eba00ce3755247833
otkken2 <lovelovezamakko2@gmail.com>
2025-03-23 09:00:10 +0900

    編集画面にてメモ詳細の取得と、メモの編集、更新機能を実装

--- src/app/memo/edit.tsx
+++ src/app/memo/edit.tsx
@@ -2,27 +2,60 @@ import {
   View,
   StyleSheet,
   TextInput,
-  KeyboardAvoidingView
+  Alert
 } from 'react-native'
 import { CircleButton } from '../../components/CircleButton'
 import { Icon } from '../../components/Icon'
-import { router } from 'expo-router'
+import { router, useLocalSearchParams } from 'expo-router'
+import { useEffect, useState } from 'react'
+import { db, auth } from '../../config'
+import { doc, getDoc, setDoc, Timestamp } from 'firebase/firestore'
+import KeyboardAvoidingView from '../../components/KeyboardAvoidingView'
 
-const handlePress = () => {
-  router.back()
+
+const handlePress = (id: string, bodyText: string) => {
+  if (auth.currentUser === null) return
+  const ref = doc(db, `users/${auth.currentUser.uid}/memos`, id)
+  setDoc(ref, {
+    bodyText,
+    updatedAt: Timestamp.fromDate(new Date())
+  }).then(() => {
+    router.back()
+  }).catch((error) => {
+    console.log(error)
+    Alert.alert('更新に失敗しました')
+  })
 }
 
 const Edit = () => {
+  const [bodyText, setBodyText] = useState<string>('')
+  const id = String(useLocalSearchParams().id)
+
+  useEffect(() => {
+    // データの取得から
+    if (auth.currentUser === null) return
+    const ref = doc(db, `users/${auth.currentUser.uid}/memos`, id)
+    getDoc(ref).then((docRef) => {
+      const remoteBodyText = docRef?.data()?.bodyText
+      setBodyText(remoteBodyText)
+    }).catch((error) => {
+      console.log(error)
+    })
+  }, [])
+
   return (
-    <KeyboardAvoidingView style={styles.container} behavior='height'>
+    <KeyboardAvoidingView style={styles.container}>
       <View style={styles.inputContainer}>
         <TextInput
-          value={'買い物\n\nリスト'}
+          value={bodyText}
           style={styles.input}
           multiline
+          onChangeText={(text) => {
+            setBodyText(text)
+          }}
         />
       </View>
-      <CircleButton onPress={handlePress}>
+      <CircleButton onPress={() => handlePress(id, bodyText)}>
         <Icon name="check" size={40} color="white" />
       </CircleButton>
     </KeyboardAvoidingView>
@@ -34,15 +67,15 @@ const styles = StyleSheet.create({
     flex: 1
   },
   inputContainer: {
-    paddingVertical: 32,
-    paddingHorizontal: 27,
     flex: 1
   },
   input: {
     flex: 1,
     textAlignVertical: 'top',
     fontSize: 16,
-    lineHeight: 24
+    lineHeight: 24,
+    paddingVertical: 32,
+    paddingHorizontal: 27
   }
 })
 
